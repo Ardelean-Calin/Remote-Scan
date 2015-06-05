fd60cff41707c69842cec4d7b5fdbd47c4fe4ed7
acpcalin <acp.calin@gmail.com>
2015-06-05 15:07:50 +0300

    Added readme and configuration file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-I will add a README tomorrow
+Remote Scan is a simple webapp written in Python3 and Flask, with the help of
+which you can access a scanner connected either to your network or directly to
+the PC. You must run the app on the PC from which you want to send the scan
+command and on which you want to get the scanned images.
+The app will be accessible from any device connected to the local network.
+
+Requirments:
+- Linux based OS
+- Python 3
+- Flask
+- scanimage (`sudo apt-get install scanimage` on Debian based systems)
+
+Instructions:
+- Make sure all the dependencies are satisfied
+- Open a terminal and run `scanimage --list` to get a list of the available
+  scanners.
+<img src="http://i.imgur.com/VUF9zwa.png">
+- Copy the name of the desired device(without quotes). In my case:
+  *hpaio:/net/Deskjet_3050_J610_series?zc=HP0BF9DD*
+- Open *scanconf.conf* and paste your copied device name right next to the
+  *device-name* entry
+- Edit the output-dir in *scanconf.conf* to match your desired output directory
+  for the scanned images
+- Optionally: edit the resolution and color of the scan
+<img src="http://i.imgur.com/rgVP5qV.png">
+- You're done! Make sure your scanner is online and run `python3 main.py`
+  A server will be created on the local network at port 5000. To access the
+  webapp from any other device you have to know the local IP of your computer
+  and enter that IP in the web browser from which you wish to connect, followed
+  by the port number: *5000*
+    Ex: http://192.164.0.1:5000
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from flask import render_template
 from flask import request
 import os
 import shutil
+import re
 
 count = 0
+device = "";
+resolution = "";
+color = "";
 scanDir = "/home/calin/Documents/Scanner"
 
 app = Flask(__name__)
@@ -22,8 +26,8 @@ def hello(name = None):
 def scanCmd():          # se executa la GET request. cand se incarca pagina
     global count
     print("Scan initiated")
-    command = "scanimage --device-name=hpaio:/net/Deskjet_3050_J610_series?zc=HP0BF9DD --format=tiff -\
--mode=Gray --resolution=200 >{:d}.tiff".format(count)
+    command = "scanimage --device-name={:s} --format=tiff -\
+-mode={:s} --resolution={:s} >{:d}.tiff".format(device, color, resolution, count)
 
     os.system(command)
 
@@ -36,5 +40,27 @@ def resetCmd():
     count = 0
     return render_template("reset.html")
 
+def extractConf():
+    f = open("scanconf.conf", 'r')
+    f_content = f.read()
+    f.close()
+
+    global device, resolution, color, scanDir
+
+    device = re.split(r'device-name\s?=\s?', \
+    re.findall(r'device-name.*=.*\n', f_content)[0])[1].split('\n')[0]
+
+    resolution = re.split(r'resolution\s?=\s?', \
+    re.findall(r'resolution.*=.*\n', f_content)[0])[1].split('\n')[0]
+
+    color = re.split(r'color\s?=\s?', \
+    re.findall(r'color.*=.*\n', f_content)[0])[1].split('\n')[0]
+
+    scanDir = re.split(r'output-dir\s?=\s?', \
+    re.findall(r'output-dir.*=.*\n', f_content)[0])[1].split('\n')[0]
+
+
+
 if __name__ == "__main__":
+    extractConf() # load the configuration variables from the file
     app.run(host='0.0.0.0')#vizibil tuturor din reteaua locala
